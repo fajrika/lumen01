d7181b0f24c8060668be54fbd264af886bc502c7
fajrika <rfajrika22@gmail.com>
2021-03-14 09:11:28 +0700

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function login(Request $request)
+    {
+        if (!$request->username)
+            return response()->json([
+                'message' => 'User belum di isi'
+            ], 400);
+        else if (!$request->password)
+            return response()->json([
+                'message' => 'Password belum di isi'
+            ], 400);
+        else {
+            $results = DB::select("SELECT * FROM [user]");
+            return response()->json([
+                'message' => $results
+
+            ], 200);
+        }
+    }
+    //
+}
